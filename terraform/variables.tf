1336217bb6e791f4e75d0c5d3736760bbc180b7c
minnikautonen <minnikautonen@edu.turkuamk.fi>
2025-09-25 12:23:25 +0800

    Add Terraform IAM configuration and provider setup

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,5 @@
 variable "aws_region" {
   description = "The AWS region where resources will be created"
   type        = string
-  # choosing eu-north-1 (Stockholm) as it's typically the cheapest region in Europe
-  # it provides good performance for our Finnish data source (Digitraffic API)
-  default     = "eu-north-1"
+  default     = "us-east-1"
 }
